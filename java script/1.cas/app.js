ab0932a7569709f8a283fc3b078ebbf539c53e5d
muhamedr <mresidagic07@gmail.com>
2023-11-22 21:25:35 +0100

    osnove

---
+++ java script/1.cas/app.js
@@ -0,0 +1,18 @@
+naslov = document.getElementById("naslov");
+naslov.innerHTML = "Naslov ispisan preko JavaScripta";
+naslov.style = "color: red; font-size: 52px";
+
+// TIPOVI PODATAKA
+
+
+// Da bismo znali tip promenljive u JavaScriptu koristimo typeof operator.
+// Tipovi podataka u JavaScriptu se dele u dve grupe:
+// 1. Primitivni tipovi podataka (Vrednosni)
+// 2. Neprimitivni tipovi podataka (Referentni)
+
+// 1.1. Stringovi - Niz karaktera koji je zapisan pod navodnicima.Obicnim ili duplim.
+console.log("ovo je nas prvi String.");
+console.log(typeof "ovo je nas prvi String.");
+
+// 1.2. number - predstavlja broj i on moze biti zapisan sa ili bez decimalnog zareza.
+
