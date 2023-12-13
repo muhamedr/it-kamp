6520bd3f0b9b15ba19591dc815fa75f234472789
muhamedr <mresidagic07@gmail.com>
2023-12-13 18:57:38 +0100

    poruka

---
+++ java script/dom/app.js
@@ -0,0 +1,44 @@
+// Domaci:
+// 1.
+// Ispisati neparne brojeve od 1 do 20 na dva nacina.
+// 2.
+// Ispisati parne brojeve od 1 do 20 na dva nacina.
+// 3.
+// Ispisati dvostruku vrednost prirodnih brojeva od 6 do 14.
+// 4.
+// Ispisati sve brojeve od 50 do 100 koji su deljivi sa 5.
+
+// 1.1
+for (let broj = 1; broj <= 20; broj += 2) {
+  console.log(broj);
+}
+
+// 1.2
+
+for (let broj = 1; broj <= 20; broj++) {
+  if (broj % 2 === 0) {
+    continue;
+  }
+  console.log(broj);
+}
+
+// 2.1
+
+for (let broj = 2; broj <= 20; broj += 2) {
+  console.log(broj);
+}
+
+// 3.1
+
+for (let broj = 6; broj <= 14; broj++) {
+  let dvostruko = broj * 2;
+  console.log(`Dvostruka vrijednost broja ${broj} je ${dvostruko}`);
+}
+
+// 4
+
+for (let broj = 50; broj <= 100; broj++) {
+  if (broj % 5 === 0) {
+    console.log(broj);
+  }
+}
